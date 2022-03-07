793658463474997803d70846d809c040bf8c00fc
CharlesMace <55921248+CharlesMace@users.noreply.github.com>
2022-03-07 11:04:43 -0500

    oui

--- test/Exercices/Step2Exercice.cpp
+++ test/Exercices/Step2Exercice.cpp
@@ -1,23 +1,21 @@
 #include <Arduino.h>
 
-//#define pinEnable 13 // Activation du driver/pilote
 #define XpinStep    2 // Signal de PAS (avancement)
 #define XpinDir     5 // Direction 
 #define YpinStep    3 // Signal de PAS (avancement)
 #define YpinDir     6 // Direction 
 
-volatile int interrupt = 0; 
-
-int y=0;
-int x=0;
-int step_x = 0;
-int step_y = 0;
-int pause_x = 0;
-int pause_y = 0;
+unsigned int y=0;
+unsigned int x=0;
+unsigned int step_x = 0;
+unsigned int step_y = 0;
+unsigned int pause_x = 0;
+unsigned int pause_y = 0;
 
 void setup(){
   Serial.begin(9600);
   Serial.println("Test A4988");
+  
   //pinMode( pinEnable, OUTPUT );
   pinMode( XpinDir   , OUTPUT );
   pinMode( XpinStep  , OUTPUT );
@@ -28,16 +26,10 @@ void setup(){
   digitalWrite( XpinStep  , LOW);  // Initialisation de la broche step
   digitalWrite( YpinDir   , HIGH); // Direction avant
   digitalWrite( YpinStep  , LOW);  // Initialisation de la broche step
-
-  cli();
-  TCCR2A = 0b00000010;
-  TCCR2B = 0b00000110; // clock/256 soit 16 microsecodes
-  TIMSK2 = 0b00000010;
-  OCR2A = 32; // 16usec * 31 = 0,469msec (je fais un toggle de la patte step, ce qui fait une frequence de la moitiÃ©, donc une periode doubl de environ 1msec
-  sei();
+  delay(10);
 }
 
-ISR(TIMER2_COMPA_vect) {
+void loop(){
   int X_step_number = 9600;
   int Y_step_number = 12800;
   int X_frequency = 40;
@@ -80,7 +72,5 @@ ISR(TIMER2_COMPA_vect) {
   y++;
 
   delayMicroseconds(50);
-}
 
-void loop(){
 }
--- test/Exercices/Step3_1.cpp
+++ test/Exercices/Step3_1.cpp
@@ -6,18 +6,7 @@
 #define YpinStep    3 // Signal de PAS (avancement)
 #define YpinDir     6 // Direction 
 
-volatile int interrupt = 0; 
-
-int y=0;
-int x=0;
-int step_x = 0;
-int step_y = 0;
-int pause_x = 0;
-int pause_y = 0;
-
 void setup(){
-  Serial.begin(9600);
-  Serial.println("Test A4988");
   //pinMode( pinEnable, OUTPUT );
   pinMode( XpinDir   , OUTPUT );
   pinMode( XpinStep  , OUTPUT );
@@ -38,48 +27,8 @@ void setup(){
 }
 
 ISR(TIMER2_COMPA_vect) {
-  int X_step_number = 9600;
-  int Y_step_number = 12800;
-  int X_frequency = 40;
-  int Y_frequency = 25;
-
-  if (step_x<X_step_number) {
-    if (x==X_frequency){
-      x = 0;
-      digitalWrite( XpinStep, HIGH );
-      digitalWrite( XpinStep, LOW );
-      step_x++;
-    }
-  }
-  else {
-    pause_x++;
-    if (pause_x == 60000){// 3sec = 3000000usec/50usec => 60000
-      pause_x = 0;
-      step_x = 0;
-      x = 0;
-    }
-  }
-
-  if (step_y<Y_step_number) {
-    if (y==Y_frequency){
-      y = 0;
-      digitalWrite( YpinStep, HIGH );
-      digitalWrite( YpinStep, LOW );
-      step_y++;
-    }
-  }
-  else {
-    pause_y++;
-    if (pause_y == 60000){ // 3sec = 3000000usec/50usec => 60000
-      pause_y = 0;
-      step_y = 0;
-    }
-  }
-
-  x++;
-  y++;
-
-  delayMicroseconds(50);
+  digitalWrite(YpinStep,!digitalRead(YpinStep));
+  digitalWrite(XpinStep,!digitalRead(XpinStep));
 }
 
 void loop(){
