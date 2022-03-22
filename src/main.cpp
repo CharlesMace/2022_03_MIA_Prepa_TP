17edbfb37f26ef241b79b99e289f04ffa1a3e03c
CharlesMace <55921248+CharlesMace@users.noreply.github.com>
2022-03-22 11:42:38 -0500

    la flemme

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,6 @@ int IR_04 = A1; // Hold on CNC shield
 // The colour sensor is connected to the SDA/SCL pins of the Arduino (A4 abd A5)
 // ****************************************************************************
 
-char SensorSimulation;
-
 int CompteurSimulation = 0;
 int r = 0;
 int g = 0;
@@ -28,33 +26,39 @@ void setup() {
 }
 
 void loop() {
-  while (CompteurSimulation < 4) {
       if (Serial.available()) {
-        SensorSimulation = Serial.read();
-        /*
-        * Please, select the 'no new line' option on the Serial Monitor
-        * in the bottom right box
-        */
+        Serial.println("What is the value of r?");
+        r = Serial.read();
+        delay(2000);
         Serial.print("The character received is: ");
-        Serial.print(SensorSimulation);
+        Serial.print(r);
         Serial.println();
-        if (r != 0 || g != 0 || b != 0 || c != 0){
-          CompteurSimulation++;
-        }
+        Serial.println("What is the value of g?");
+        g = Serial.read();
+        delay(2000);
+        Serial.println("The character received is: ");
+        Serial.print(g);
+        Serial.println();
+        Serial.println("What is the value of b?");
+        b = Serial.read();
+        delay(2000);
+        Serial.println("The character received is: ");
+        Serial.print(b);
+        Serial.println();
+        Serial.println("What is the value of c?");
+        c = Serial.read();
+        delay(2000);
+        Serial.println("The character received is: ");
+        Serial.print(c);
+        Serial.println();
       }
+    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+    Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+    Serial.print("IR 01: "); Serial.print(digitalRead(IR_01)); Serial.print(" ");
+    Serial.print("IR 02: "); Serial.print(digitalRead(IR_02)); Serial.print(" ");
+    Serial.print("IR 03: "); Serial.print(digitalRead(IR_03)); Serial.print(" ");
+    Serial.print("IR 04: "); Serial.print(digitalRead(IR_04));
     delay(500); 
-  }
-
-  unsigned long t0,t;
-  t0 = millis();
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.print("IR 01: "); Serial.print(digitalRead(IR_01)); Serial.print(" ");
-  Serial.print("IR 02: "); Serial.print(digitalRead(IR_02)); Serial.print(" ");
-  Serial.print("IR 03: "); Serial.print(digitalRead(IR_03)); Serial.print(" ");
-  Serial.print("IR 04: "); Serial.print(digitalRead(IR_04)); Serial.print(" ");
-  t = millis() - t0; // t is a measurement of the time spent
-  Serial.println(t);
 }
